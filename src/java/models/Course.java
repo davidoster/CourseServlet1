8a642d189d98df5bc46e75e89b1ed0d4e3a73a92
David Oster <paspa@hotmail.com>
2020-04-23 14:10:52 +0300

    Latest Code

---
+++ src/java/models/Course.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.Date;
+
+
+/**
+ *
+ * @author mac
+ */
+public class Course {
+
+    
+    private String title;
+    private String stream;
+    private String type;
+    private Date start_date;
+    private Date end_date;
+    
+
+    @Override
+    public String toString() {
+        return "Title of Course : " + title + ", Stream Of Course : " + stream + ", Type Of Course : " + type + ", Starting Date : " + start_date + ", End Date : " + end_date + '}' + "\n";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStream() {
+        return stream;
+    }
+
+    public void setStream(String stream) {
+        this.stream = stream;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+    
+}
---
+++ web/WEB-INF/views/course.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : course
+    Created on : 23-Apr-2020, 11:51:27
+    Author     : mac
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Course</title>
+    </head>
+    <body>
+        <h1>Course Data</h1>
+        <%
+            RequestDispatcher rd = request.getRequestDispatcher("../../menu.html");
+            rd.include(request, response);
+            out.print("Title : " + request.getParameter("title"));
+            out.print("Username : " + session.getAttribute("username"));
+        %>
+    </body>
+</html>
